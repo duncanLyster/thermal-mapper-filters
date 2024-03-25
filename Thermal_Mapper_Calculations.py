dedb4ccecdac57cfe9ad869c322d6a9556efbe28
Duncan Lyster <duncan.lyster@physics.ox.ac.uk>
2024-03-25 11:01:19 +0000

    Created repository and .py file with just a header comment for now

---
+++ Thermal_Mapper_Calculations.py
@@ -0,0 +1,5 @@
+''' 
+This script is built for performing filter calculations for the Enceladus Thermal Mapper (ETM) instrument.
+It reads in mission configuration info from a .json file, filter assembly configuration info from a .json file, and a .csv file containing the filter transmission data.
+It then calculates the filter transmission for each filter assembly, and signal-to-noise ratio for each filter assembly in a range of wavelengths/scenarios. 
+'''
